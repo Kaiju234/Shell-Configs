c62feeb8b3b2f689841870991e48a29b8ad51c66
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2023-11-14 12:49:26 -0500

    Add files via upload
    This is my Powershell Configuration

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh | Invoke-Expression
+
+oh-my-posh init pwsh --config 'C:\Users\rajas\AppData\Local\Programs\oh-my-posh\themes\plague.omp.json' | 
+Invoke-Expression
+
+Set-Alias neofetch pwshfetch-test-1
+
+winfetch
